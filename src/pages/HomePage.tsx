6725dff54b7af05253c6c3d58641d3ec124289bb
imam0321 <imam.hossain0321@gmail.com>
2025-08-22 17:38:32 +0600

    Fix: Margin problem

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,19 @@
-<<<<<<< HEAD
 import Contact from "@/components/modules/Home/Contact";
 import Faq from "@/components/modules/Home/Faq";
 import Features from "@/components/modules/Home/Features";
 import Hero from "@/components/modules/Home/Hero";
 import Pricing from "@/components/modules/Home/Pricing";
 import StatsStrip from "@/components/modules/Home/StatsStrip";
-=======
-import Hero from "@/components/modules/Home/Hero";
-
->>>>>>> b9d87debd640d5f40383c2ca534610f5bcb2bd0b
 
 export default function HomePage() {
   return (
     <>
-<<<<<<< HEAD
       <Hero />
       <Features />
       <Pricing />
       <StatsStrip />
       <Faq />
       <Contact />
-=======
-      <Hero/>
-    
->>>>>>> b9d87debd640d5f40383c2ca534610f5bcb2bd0b
     </>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,6 @@
 import App from "@/App";
-<<<<<<< HEAD
-import AboutPage from "@/pages/AboutPage";
-=======
->>>>>>> b9d87debd640d5f40383c2ca534610f5bcb2bd0b
 import HomePage from "@/pages/HomePage";
+import AboutPage from "@/pages/AboutPage";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
@@ -12,7 +9,6 @@ export const router = createBrowserRouter([
     path: "/",
     children: [
       {
-<<<<<<< HEAD
         index: true,
         Component: HomePage,
         path: "/",
@@ -20,11 +16,6 @@ export const router = createBrowserRouter([
       {
         Component: AboutPage,
         path: "/about",
-=======
-        Component: HomePage,
-        path: "/",
-        index: true,
->>>>>>> b9d87debd640d5f40383c2ca534610f5bcb2bd0b
       },
     ],
   },
